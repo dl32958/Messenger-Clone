d3f6b5210e222ffef1a80342d62967e675c61f76
Dong Lu <144203360+dl32958@users.noreply.github.com>
2024-12-23 15:47:06 -0800

    add sidebar and nav layout

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  appDir: true,
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+    ],
+  }
 };
 
 export default nextConfig;
